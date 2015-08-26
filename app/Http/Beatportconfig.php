2d59a3f9b87295c7bb110c0055e1d16fbdfba641
MartinLavanant <martin.l@apps-panel.com>
2015-08-26 15:07:34 +0200

    beatport

---
+++ app/Http/Beatportconfig.php
@@ -0,0 +1,9 @@
+<?php
+
+define('CONSUMER', 'c7017a7503f52e5519402db91ae34382a3a8c4e8');
+
+define('SECRET', '1ce76966da0d68bcebb5cb5a6d42162b204f0345');
+
+define('LOGIN', 'martinlavanant');
+define('PASSWORD', 'redline231192');
+?>
