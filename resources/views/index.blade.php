5e6011ccc0970a38a9e5493381d29f51ba13a74f
sylvaindev <sylvain.martin@epitech.eu>
2015-08-26 11:50:30 +0200

    add layout + index + route

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="content">
+  <div class="content-heading">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-lg-push-3 col-sm-10 col-sm-push-1">
+          <h1 class="heading hidden-xs hidden-xx">Music World</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 col-lg-push-3 col-sm-10 col-sm-push-1">
+        <section class="content-inner">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <blockquote>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed vestibulum risus, in dictum ex. Duis faucibus accumsan lectus, sed lacinia nisi sodales vel. Aenean pulvinar sed enim eu blandit. Integer ante enim, ornare a magna semper, rhoncus porttitor metus. Morbi id enim a tortor bibendum consectetur. Nulla fermentum, quam non luctus viverra, lacus purus auctor turpis, vitae maximus diam justo ac libero. Morbi vestibulum finibus velit, id tristique nulla finibus in. Donec a finibus massa, sed faucibus ligula.</p>
+            <p>Cras finibus lectus ac facilisis dapibus. Curabitur aliquet ligula sed elit tempus scelerisque. Aliquam neque est, rutrum quis ipsum porttitor, posuere ullamcorper eros. In tincidunt congue odio, eu ultricies ex congue eu. Pellentesque vitae nunc sit amet ex egestas posuere porta eget velit. Sed id mauris ultrices, pretium leo a, ultricies nisl. Donec libero felis, accumsan at velit et, finibus commodo lectus. Etiam ornare arcu eget tincidunt porta. Integer lobortis auctor nisi, vel congue lectus consequat ac. Fusce molestie varius pulvinar. Duis quis consectetur nibh. Quisque dictum diam at quam fringilla tempus. Nunc nec viverra justo.</p>
+            <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum lacinia mollis iaculis. Pellentesque aliquet lectus vitae sapien scelerisque vestibulum. Nullam non erat laoreet, tincidunt nibh tincidunt, imperdiet nisi. Pellentesque vitae leo vitae ligula vehicula dignissim quis in nisl. Maecenas luctus nisl ipsum, nec vehicula dui ullamcorper eget. Curabitur porttitor eget eros et convallis. Suspendisse pharetra nibh ut faucibus euismod. Nunc erat libero, dignissim vitae posuere sed, pretium posuere ex. Nam a pharetra odio, id sollicitudin purus.</p>
+          </blockquote>
+        </section>
+      </div>
+    </div>
+  </div>
+</div>
+@stop
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta content="IE=edge" http-equiv="X-UA-Compatible">
+	<meta content="initial-scale=1.0, width=device-width" name="viewport">
+	<title>Music World</title>
+
+	<!-- css -->
+	<link href="css/material-design.css" rel="stylesheet">
+
+	<!-- css for this project -->
+	<link href="css/styles.css" rel="stylesheet">
+
+	<!-- favicon -->
+	<!-- ... -->
+
+	<!-- ie -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+</head>
+<!-- Loader -->
+<body class="avoid-fout">
+	<div class="avoid-fout-indicator avoid-fout-indicator-fixed">
+		<div class="progress-circular progress-circular-alt progress-circular-center">
+			<div class="progress-circular-wrapper">
+				<div class="progress-circular-inner">
+					<div class="progress-circular-left">
+						<div class="progress-circular-spinner"></div>
+					</div>
+					<div class="progress-circular-gap"></div>
+					<div class="progress-circular-right">
+						<div class="progress-circular-spinner"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Menu -->
+	<header class="header header-transparent header-waterfall">
+		<ul class="nav nav-list pull-left">
+			<li>
+				<a data-toggle="menu" href="#menu">
+					<span class="icon icon-lg">menu</span>
+				</a>
+			</li>
+		</ul>
+		<a class="header-logo margin-left-no" data-toggle="menu" href="#menu">
+			<img src="images/logo/logo.png"></img>
+		</a>
+		<!-- Page indicator -->
+		<div class="header-affix pull-left" data-offset-top="108" data-spy="affix">
+			<span class="header-text margin-left-no">
+				<i class="icon margin-right">chevron_right</i>Home
+			</span>
+		</div>
+		<!-- Right menu profile -->
+		<ul class="nav nav-list pull-right">
+			<li>
+				<a data-toggle="menu" href="#profile">
+					<span class="access-hide">Sylvain Martin</span>
+					<span class="avatar"><img src="images/users/avatar-001.jpg"></span>
+				</a>
+			</li>
+		</ul>
+	</header>
+	<!-- Left menu -->
+	<nav aria-hidden="true" class="menu" id="menu" tabindex="-1">
+		<div class="menu-scroll">
+			<div class="menu-content">
+
+				<!-- Search bar -->
+				<div class="media menu-logo">
+					<div class="media-object pull-left">
+						<label class="form-icon-label" for="input-search"><span class="icon">search</span></label>
+					</div>
+					<div class="media-inner">
+						<input class="form-control" id="input-search" placeholder="RECHERCHE" type="text">
+					</div>
+				</div>
+
+				<ul class="nav">
+					<li>
+						<a class="waves-attach" href="/index.html"><span class="icon icon-lg">home</span>Home</a>
+					</li>
+					<li>
+						<a class="waves-attach menu-collapse collapsed" data-target="#genre" data-toggle="collapse" href="#"><span class="icon icon-lg">view_list</span>Genres</a>
+						<span class="menu-collapse-toggle collapsed" data-target="#genre" data-toggle="collapse"><i class="icon menu-collapse-toggle-close">close</i><i class="icon menu-collapse-toggle-default">add</i></span>
+						<ul class="menu-collapse collapse" id="genre">
+							<li>
+								<a class="waves-attach" href="templates/electro.html">Electro</a>
+							</li>
+							<li>
+								<a class="waves-attach" href="#">Hip Hop</a>
+							</li>
+							<li>
+								<a class="waves-attach" href="#">Rock</a>
+							</li>
+						</ul>
+					</li>
+			</div>
+		</div>
+	</nav>
+	<!-- Right menu -->
+	<nav aria-hidden="true" class="menu menu-right" id="profile" tabindex="-1">
+		<div class="menu-scroll">
+			<div class="menu-top">
+				<div class="menu-top-info">
+					<a class="menu-top-user" href="javascript:void(0)"><span class="avatar pull-left"><img src="images/users/avatar-001.jpg"></span>Sylvain Martin</a>
+				</div>
+				<div class="menu-top-info-sub">
+					<small>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</small>
+				</div>
+			</div>
+      @yield('content')
+			<!-- Footer -->
+		<footer class="footer">
+			<div class="container">
+				<p>©Copyright</p>
+			</div>
+		</footer>
+		<!-- Player button -->
+		<div class="fbtn-container">
+			<div class="fbtn-inner">
+				<a class="fbtn fbtn-green fbtn-lg" data-toggle="dropdown"><span class="fbtn-text">Player</span><span class="fbtn-ori icon">add</span><span class="fbtn-sub icon">close</span></a>
+				<div class="fbtn-dropdown">
+					<a class="fbtn" target="_blank"><span class="fbtn-text">Back</span><span class="fa fa-step-backward"></span></a>
+					<a class="fbtn" target="_blank"><span class="fbtn-text">Play</span><span class="fa fa-play"></span></a>
+					<a class="fbtn" target="_blank"><span class="fbtn-text">Pause</span><span class="fa fa-pause"></span></a>
+					<a class="fbtn" target="_blank"><span class="fbtn-text">Next</span><span class="fa fa-step-forward"></span></a>
+				</div>
+			</div>
+		</div>
+
+		<!-- js -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="js/material-design.min.js"></script>
+
+		<!-- js for this project -->
+		<script src="js/project.js"></script>
+	</body>
+	</html>
