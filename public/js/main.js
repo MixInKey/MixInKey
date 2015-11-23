228aadeca83fb425615964c392c64ec666f65b4a
Sylvain Martin <Sylvain@MacBook-Pro-de-Sylvain.local>
2015-11-23 15:15:40 +0100

    add main.js

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+});
+
+$(".button-collapse").sideNav();
+
+$(document).ready(function() {
+    $('select').material_select();
+});
