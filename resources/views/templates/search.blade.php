99d3c2e9be59f85ab59ad1f9c55c7d32a8e7a2dd
chalasr <robin.chalas@epitech.eu>
2015-11-24 17:34:59 +0100

    re-add good version of search view

--- resources/views/templates/search.blade.php
+++ resources/views/templates/search.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.master')
 
 @section('content')
-<<<<<<< HEAD
 <div class="main z-depth-1">
 	<div class="page-header">
 		<h1>Advanced Search</h1>
@@ -14,22 +13,6 @@
 				<option ng-repeat="genre in genres"><% genre.name %></option>
 			</select>
 		</div><br>
-=======
-	<base href="/search/">
-	<div class="container">
-		<div class="main z-depth-2">
-			<div class="page-header">
-				<h1>Recherche Avancée</h1>
-			</div><br>
-
-			 <div class="container">
-				 <div class="form-group form-group-label" ng-init="findGenres()">
-           <label for="genre">Genre</label>
-           <select class="form-control" id="genre">
-               <option ng-repeat="genre in genres"><% genre.name %></option>
-           </select>
-	 	</div>
->>>>>>> routing
 
 		<div class="form-group">
 			<label for="range">BPM</label>
@@ -57,18 +40,8 @@
 			</div>
 		</form><br>
 
-<<<<<<< HEAD
 		<div class="button-search bot-space">
 			<a class="waves-effect waves-light btn"><i class="material-icons left">search</i>Search</a>
-=======
-			    <div class="button-search bot-space">
-				    <button type="submit" class="waves-effect waves-light btn"><i class="material-icons left">search</i>Rechercher</button>
-			    </div>
-
-			 	<div ng-view></div>
-
-			 </div>
->>>>>>> routing
 		</div>
 	</div>
 </div>
