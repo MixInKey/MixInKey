15e6132fc6d7ccf409a009d8246035c0f13bc18a
chalasr <robin.chalas@epitech.eu>
2015-11-24 13:18:10 +0100

    remove undefined call

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
 (function(app) {
   var rangeSlider = document.getElementById('slider-range');
 
-  noUiSlider.create(rangeSlider, {
-  	start: [ 4000 ],
-  	range: {
-  		'min': [  2000 ],
-  		'max': [ 10000 ]
-  	}
-  });
+  // noUiSlider.create(rangeSlider, {
+  // 	start: [ 4000 ],
+  // 	range: {
+  // 		'min': [  2000 ],
+  // 		'max': [ 10000 ]
+  // 	}
+  // });
 
 })(angular.module('beatportApp', [
     'MainCtrl',
