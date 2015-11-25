9a100fc2e0277625ed7f3bd2c5eb3fc0055f7107
Martin <martin.l@apps-panel.com>
2015-11-25 10:12:17 +0100

    fix conflict

--- resources/views/templates/search.blade.php
+++ resources/views/templates/search.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.master')
 
 @section('content')
-<<<<<<< HEAD
 <div class="main z-depth-1">
 	<div class="page-header">
 		<h1>Advanced Search</h1>
@@ -21,7 +20,6 @@
 				<input type="range" name="range" min="0" max="100" />
 			</p>
 		</div><br>
-=======
 	<base href="/search/">
 	<div class="container">
 		<div class="main z-depth-2">
@@ -36,7 +34,6 @@
                <option ng-repeat="genre in genres"><% genre.name %></option>
            </select>
 	 	</div>
->>>>>>> "routing"
 
 				<div class="form-group">
 					<p class="range-field">
@@ -63,7 +60,6 @@
 					</div>
 			    </form><br>
 
-<<<<<<< HEAD
 		<div class="button-search bot-space">
 			<a ng-click="main.search()" class="waves-effect waves-light btn"><i class="material-icons left">search</i>Search</a>
 		</div>
@@ -73,7 +69,6 @@
 					<a id="[% track.id %]" ng-click="main.changeTrack(track.id)">[% track.id %] [% track.name %]</a>
 					<img class="thumbnail" ng-src="[% track.images.medium.url %]">
 			</div>
-=======
 			    <div class="button-search bot-space">
 				    <button type="submit" class="waves-effect waves-light btn"><i class="material-icons left">search</i>Rechercher</button>
 			    </div>
@@ -81,7 +76,6 @@
 			 	<div ng-view></div>
 
 			 </div>
->>>>>>> "routing"
 		</div>
 		<iframe ng-src="[% main.currentPlayer %]" id="playerFrame" width='800' height='166' scrolling='no' frameborder='0'></iframe>
 	</div>
