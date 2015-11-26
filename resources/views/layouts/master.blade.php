0d2969f9d30e93414ca38b52be80636ca385dce8
Sylvain <sylvain.martin@epitech.eu>
2015-11-26 15:48:17 +0100

    add search navbar and sidebar, add artists input, add key select input

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,90 +22,98 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
 	<script src="{{ URL::to('js/static/toastr.min.js') }}"></script>
 	<!-- ie -->
-		<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
 </head>
 <body class="avoid-fout" ng-controller="MainController as main">
 
 	<!-- Menu -->
-<header>
-	<ul id="dropdown1" class="dropdown-content">
-	  <li>
-	  	<a href="/"><i class="material-icons left">list</i>Playlist</a>
-	  </li>
-	  <li>
-	  	<a href="/"><i class="material-icons left">settings</i>Settings</a>
-	  </li>
-	  <li>
-	  	<a href="{{ URL::to('logout') }}" target="_self"><i class="material-icons left">exit_to_app</i>Log out</a>
-	  </li>
-	</ul>
+	<header>
+		<ul id="dropdown1" class="dropdown-content">
+			<li>
+				<a href="/"><i class="material-icons left">list</i>Playlist</a>
+			</li>
+			<li>
+				<a href="/"><i class="material-icons left">settings</i>Settings</a>
+			</li>
+			<li>
+				<a href="{{ URL::to('logout') }}" target="_self"><i class="material-icons left">exit_to_app</i>Log out</a>
+			</li>
+		</ul>
 
-	<div class="navbar-fixed">
-		<nav>
-		    <div class="nav-wrapper">
-		      <a href="/" class="brand-logo">
-			      <img src="{{ URL::to('images/logo/logo50x50.png') }}"></a>
-		      </a>
+		<div class="navbar-fixed">
+			<nav>
+				<div class="nav-wrapper">
+					<a href="/" class="brand-logo">
+						<img src="{{ URL::to('images/logo/logo50x50.png') }}"></a>
+					</a>
 					@if(Auth::check())
-		      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-		      <ul class="right hide-on-med-and-down">
-		        <li>
-		        	<a href="/" target="_self"><i class="material-icons left">home</i>Home</a>
-		        </li>
-		        <li>
-		        	<a class="dropdown-button" href="/" data-activates="dropdown1"><i class="material-icons left">account_box</i>Profile<i class="material-icons right">arrow_drop_down</i></a>
-		        </li>
-		      </ul>
-		      <ul class="side-nav" id="mobile-demo">
-			  	<li>
-		        	<a href="/" target="_self"><i class="material-icons left">home</i>Home</a>
-		        </li>
-		        <ul class="collapsible" data-collapsible="expandable">
-			        <li>
-			           <div class="collapsible-header"><i class="material-icons left">account_box</i>Profile<i class="material-icons right">arrow_drop_down</i></div>
-					   <div class="collapsible-body">
-						   <p>
-							<a href="/"><i class="material-icons left">list</i>Playlist</a>
-							<a href="/"><i class="material-icons left">settings</i>Settings</a>
-							<a href="{{ URL::to('logout') }}" target="_self"><i class="material-icons left">exit_to_app</i>Log out</a>
-						   </p>
-					   </div>
-			        </li>
-		        </ul>
-		      </ul>
+					<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+					<ul class="right hide-on-med-and-down">
+						<li>
+							<a href="/" target="_self"><i class="material-icons left">home</i>Home</a>
+						</li>
+						<li>
+							<a class="dropdown-button" href="/" data-activates="dropdown1"><i class="material-icons left">account_box</i>Profile<i class="material-icons right">arrow_drop_down</i></a>
+						</li>
+						<li class="input-field">
+							<i class="material-icons prefix">search</i>
+							<input id="icon_prefix" type="text" placeholder="search" class="validate">
+						</li>
+					</ul>
+					<ul class="side-nav" id="mobile-demo">
+						<li class="input-field">
+								<i class="material-icons prefix">search</i>
+								<input id="search" type="text" placeholder="search" class="validate">
+						</li>
+						<li>
+							<a href="/" target="_self"><i class="material-icons left">home</i>Home</a>
+						</li>
+						<ul class="collapsible" data-collapsible="expandable">
+							<li>
+								<div class="collapsible-header"><i class="material-icons left">account_box</i>Profile<i class="material-icons right">arrow_drop_down</i></div>
+								<div class="collapsible-body">
+									<p>
+										<a href="/"><i class="material-icons left">list</i>Playlist</a>
+										<a href="/"><i class="material-icons left">settings</i>Settings</a>
+										<a href="{{ URL::to('logout') }}" target="_self"><i class="material-icons left">exit_to_app</i>Log out</a>
+									</p>
+								</div>
+							</li>
+						</ul>
+					</ul>
 					@endif
-		    </div>
-		</nav>
-	</div>
-</header>
-
-<main>
-	<div class="container">
-		@yield('content')
-	</div>
-</main>
-
-<footer class="page-footer">
-	<div class="footer-copyright">
-		<div class="container">
-			© 2015 MixInKey
+				</div>
+			</nav>
 		</div>
-	</div>
-</footer>
+	</header>
+
+	<main>
+		<div class="container">
+			@yield('content')
+		</div>
+	</main>
+
+	<footer class="page-footer">
+		<div class="footer-copyright">
+			<div class="container">
+				© 2015 MixInKey
+			</div>
+		</div>
+	</footer>
 
 
 	{{-- Session notifications --}}
 	@if(Session::has('error'))
-		<script type="text/javascript">
-			toastr.error('{{ Session::get('error') }}');
-		</script>
+	<script type="text/javascript">
+	toastr.error('{{ Session::get('error') }}');
+	</script>
 	@elseif(Session::has('success'))
-		<script type="text/javascript">
-			toastr.success('{{ Session::get('success') }}');
-		</script>
+	<script type="text/javascript">
+	toastr.success('{{ Session::get('success') }}');
+	</script>
 	@endif
 
 	<!-- Libs -->
