158056ea0fcf59b88629aa6213b6ff263fb0f1f7
Sylvain <sylvain.martin@epitech.eu>
2015-11-27 09:49:58 +0100

    begin add informations in player for currentTrack

--- public/js/src/controllers/MainCtrl.js
+++ public/js/src/controllers/MainCtrl.js
@@ -110,9 +110,9 @@
          * Interact with player using search results
          * @param  {Number} trackId
          */
-        self.changePlayerTrack = function(trackId) {
+        self.changePlayerTrack = function(track) {
             var player = angular.element('#player').controller();
-            player.changeTrack(trackId);
+            player.changeTrack(track);
         };
 
         /**
